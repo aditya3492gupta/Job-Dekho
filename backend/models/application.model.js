9d5bcd3cee59567eff0ae5dd3da399f765a90477
Aditya Gupta <aditya3492gupta@gmail.com>
2024-07-27 20:14:29 +0530

    created repo

---
+++ backend/models/application.model.js
@@ -0,0 +1,21 @@
+//SCHEMA FOR THE APPLICATION MADE BY APPLICANTS
+import mongoose from "mongoose";
+
+const applicationSchema = new mongoose.Schema({
+    job: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Job',
+        required: true
+    },
+    applicant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'accepted', 'rejected'],
+        default: 'pending'
+    }
+}, { timestamps: true });
+export const Application = mongoose.model("Application", applicationSchema);
