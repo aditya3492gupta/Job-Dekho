a3a92aa2fc7160a11f779fa2b8a7bd0c55ebe9a5
Aditya Gupta <aditya3492gupta@gmail.com>
2024-08-05 01:08:57 +0530

    added more components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,10 @@ import Navbar from './components/shared/Navbar'
 import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
 import Home from './components/Home'
+import Jobs from './components/Jobs'
+import Browse from './components/Browse'
+import Profile from './components/Profile'
+import JobDescription from './components/JobDescription'
 
 const appRouter = createBrowserRouter([
   {
@@ -16,48 +20,48 @@ const appRouter = createBrowserRouter([
   {
     path: '/signup',
     element: <Signup />
-  }
-  // {
-  //   path: "/jobs",
-  //   element: <Jobs />
-  // },
-  // {
-  //   path: "/description/:id",
-  //   element: <JobDescription />
-  // },
-  // {
-  //   path: "/browse",
-  //   element: <Browse />
-  // },
-  // {
-  //   path: "/profile",
-  //   element: <Profile />
-  // },
-  // // admin ke liye yha se start hoga
-  // {
-  //   path: "/admin/companies",
-  //   element: <ProtectedRoute><Companies /></ProtectedRoute>
-  // },
-  // {
-  //   path: "/admin/companies/create",
-  //   element: <ProtectedRoute><CompanyCreate /></ProtectedRoute>
-  // },
-  // {
-  //   path: "/admin/companies/:id",
-  //   element: <ProtectedRoute><CompanySetup /></ProtectedRoute>
-  // },
-  // {
-  //   path: "/admin/jobs",
-  //   element: <ProtectedRoute><AdminJobs /></ProtectedRoute>
-  // },
-  // {
-  //   path: "/admin/jobs/create",
-  //   element: <ProtectedRoute><PostJob /></ProtectedRoute>
-  // },
-  // {
-  //   path: "/admin/jobs/:id/applicants",
-  //   element: <ProtectedRoute><Applicants /></ProtectedRoute>
-  // },
+  },
+  {
+    path: "/jobs",
+    element: <Jobs />
+  },
+  {
+    path: "/description/:id",
+    element: <JobDescription />
+  },
+  {
+    path: "/browse",
+    element: <Browse />
+  },
+  {
+    path: "/profile",
+    element: <Profile />
+  },
+  // admin ke liye yha se start hoga
+  {
+    path: "/admin/companies",
+    element: <ProtectedRoute><Companies /></ProtectedRoute>
+  },
+  {
+    path: "/admin/companies/create",
+    element: <ProtectedRoute><CompanyCreate /></ProtectedRoute>
+  },
+  {
+    path: "/admin/companies/:id",
+    element: <ProtectedRoute><CompanySetup /></ProtectedRoute>
+  },
+  {
+    path: "/admin/jobs",
+    element: <ProtectedRoute><AdminJobs /></ProtectedRoute>
+  },
+  {
+    path: "/admin/jobs/create",
+    element: <ProtectedRoute><PostJob /></ProtectedRoute>
+  },
+  {
+    path: "/admin/jobs/:id/applicants",
+    element: <ProtectedRoute><Applicants /></ProtectedRoute>
+  },
 
 ])
 
